69e578b258fd712216cc326e31b6019ac1de2d79
Nicolas Biddinger <nb755524@ohio.edu>
2025-09-05 15:33:21 -0400

    no wincluding a programgit status

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nicolas Biddinger
+ *        Date: 5 September 2025
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
